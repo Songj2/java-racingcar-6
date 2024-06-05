aa8082f5cc259997e6859829e9842b26a87dddd8
SongJIH <jh2song@naver.com>
2024-06-05 21:36:43 +0900

    사용자에게 자동차 이름 입력받고 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 //        사용자에게 자동차 이름 입력받기 _IllegalArgumentException
+        String[] carName= Console.readLine().split(",");
+        Set<String> nameSet = new HashSet<>();
 //        5자 이하
+        for(String name: carName){
+            if (name.length()>5){
+                throw new IllegalArgumentException("이름의 길이를 초과하였습니다.");
+            }else {
+                nameSet.add(name);
+            }
+        }
 //        중복 확인
-
+        if(nameSet.size()!= carName.length){
+            throw new IllegalArgumentException("이름이 중복되었습니다.");
+        }
 //        사용자에게 시도 회수 입력받기 _IllegalArgumentException
 //        숫자 확인
 
