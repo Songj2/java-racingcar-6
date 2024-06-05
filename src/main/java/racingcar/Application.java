3d02e66174d1148d3b10fb7c0bfa039de7704434
SongJIH <jh2song@naver.com>
2024-06-05 21:27:59 +0900

    구현사항 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,17 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+//        사용자에게 자동차 이름 입력받기 _IllegalArgumentException
+//        5자 이하
+//        중복 확인
+
+//        사용자에게 시도 회수 입력받기 _IllegalArgumentException
+//        숫자 확인
+
+//        게임시행
+//        랜덤값 뽑기
+//        전진 조건 확인 _>=4
+//        출력 _이름:
+//        경기 종료후 우승자 발표 (중복 가능)
     }
 }
