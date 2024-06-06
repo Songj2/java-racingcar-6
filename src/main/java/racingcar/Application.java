7ef9f97b7a8c13d9dd9f6815a018e071104dabd9
SongJIH <jh2song@naver.com>
2024-06-06 15:00:29 +0900

    메서드 분리 및 이동횟수 입력및 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,33 +3,61 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        사용자에게 자동차 이름 입력받기 _IllegalArgumentException
-        String[] carName= Console.readLine().split(",");
-        Set<String> nameSet = new HashSet<>();
+        String[] carName= inputCarName();
+        validate(!checkNameSize(carName));
+        validate(!duplicatedName(carName));
+
+        String userNumber= inputCount();
+        validate(!checkInteger(userNumber));
+        int count= parseToInteger(userNumber);
+    }
+//        사용자에게 자동차 이름 입력받기
+    private static String[] inputCarName(){
+        return Console.readLine().split(",");
+    }
 //        5자 이하
-        for(String name: carName){
+    private static boolean checkNameSize(String[] carName){
+        for (String name: carName){
             if (name.length()>5){
-                throw new IllegalArgumentException("이름의 길이를 초과하였습니다.");
-            }else {
-                nameSet.add(name);
+                return false;
             }
         }
+        return true;
+    }
 //        중복 확인
-        if(nameSet.size()!= carName.length){
-            throw new IllegalArgumentException("이름이 중복되었습니다.");
+        private static boolean duplicatedName(String[] carName){
+        Set<String> nameSet= new HashSet<>(List.of(carName));
+        return nameSet.size()== carName.length;
+    }
+//    IllegalArgumentException
+    private  static void validate(boolean status){
+        if (status){
+            throw new IllegalArgumentException();
         }
+    }
 //        사용자에게 시도 회수 입력받기 _IllegalArgumentException
+    private static String inputCount(){
+        return Console.readLine();
+    }
 //        숫자 확인
-
+private static boolean checkInteger(String str){
+        if (str.matches("^\\d$")){
+            return true;
+        }
+        return false;
+}
+private static  int parseToInteger(String str){
+        return Integer.parseInt(str);
+}
 //        게임시행
 //        랜덤값 뽑기
 //        전진 조건 확인 _>=4
 //        출력 _이름:
 //        경기 종료후 우승자 발표 (중복 가능)
-    }
 }
